b372c98881a6e5b757bce7dcc00be88d24b67eb6
kd3226 <kd3226@tux1.cci.drexel.edu>
2025-02-12 16:07:11 -0500

    implemented the functions on both files

--- assignments/3-ShellP1/starter/dshlib.c
+++ assignments/3-ShellP1/starter/dshlib.c
@@ -32,8 +32,63 @@
  *  Standard Library Functions You Might Want To Consider Using
  *      memset(), strcmp(), strcpy(), strtok(), strlen(), strchr()
  */
-int build_cmd_list(char *cmd_line, command_list_t *clist)
-{
-    printf(M_NOT_IMPL);
-    return EXIT_NOT_IMPL;
+ int build_cmd_list(char *cmd_line, command_list_t *clist) {
+    memset(clist, 0, sizeof(command_list_t));
+    if(strlen(cmd_line) == 0) {
+        return WARN_NO_CMDS;
+    }
+
+    char cmd_buffer[SH_CMD_MAX];
+    strcpy(cmd_buffer, cmd_line);
+
+    char *save_ptr;
+    char *cmd_segment = strtok_r(cmd_buffer, PIPE_STRING, &save_ptr);
+    
+    while (cmd_segment) {
+        if (clist->num >= CMD_MAX) {
+            return ERR_TOO_MANY_COMMANDS;
+        }
+
+        char trimmed_cmd[SH_CMD_MAX];
+        strncpy(trimmed_cmd, cmd_segment, SH_CMD_MAX - 1);
+        trimmed_cmd[SH_CMD_MAX - 1] = '\0';
+        
+        char *cmd_start = trimmed_cmd;
+        while (isspace((unsigned char)*cmd_start)) cmd_start++;
+
+        char *cmd_end = cmd_start + strlen(cmd_start) - 1;
+        while (cmd_end > cmd_start && isspace((unsigned char)*cmd_end)) {
+            *cmd_end = '\0';
+            cmd_end--;
+        }
+
+        if (*cmd_start) {
+            char *save_ptr2;
+            char *cmd_name = strtok_r(cmd_start, " ", &save_ptr2);
+            if (cmd_name) {
+                if (strlen(cmd_name) >= EXE_MAX) {
+                    return ERR_CMD_OR_ARGS_TOO_BIG;
+                }
+                strncpy(clist->commands[clist->num].exe, cmd_name, EXE_MAX - 1);
+                
+                char *cmd_args = strtok_r(NULL, "", &save_ptr2);
+                if (cmd_args) {
+                    while (isspace((unsigned char)*cmd_args)) cmd_args++;
+                    if (strlen(cmd_args) >= ARG_MAX) {
+                        return ERR_CMD_OR_ARGS_TOO_BIG;
+                    }
+                    strncpy(clist->commands[clist->num].args, cmd_args, ARG_MAX - 1);
+                }
+                clist->num++;
+            }
+        }
+        cmd_segment = strtok_r(NULL, PIPE_STRING, &save_ptr);
+    }
+
+    if(clist->num == 0) {
+        return WARN_NO_CMDS;
+    }
+
+    return OK;
 }
+
